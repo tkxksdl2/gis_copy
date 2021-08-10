5339f9e56c3675c385a3b3d2b448e375f1102406
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-10 17:44:56 +0900

    gis 08/10 6ban reupload practice 3. commentapp

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from commentapp.views import CommentCreationView
+from commentapp.views import CommentCreationView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns = [
     path('create/', CommentCreationView.as_view(), name='create'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete'),
 
 ]
