58e2342ab85719df1cfa252e2a336bffac338546
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-23 11:46:07 +0900

    gis 08/23 6ban reupload practice 1. subscriptionapp

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
 
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscription'
 
 urlpatterns = [
-
+    path('subscribe/<int:project_pk>', SubscriptionView.as_view(), name='subscription'),
 
 ]
