a353692014c52da60a371683486d20699a9a9130
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-26 11:30:29 +0900

    gis 08/26 6ban reupload practice 1. likesapp

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.http import  HttpResponseRedirect
 from django.shortcuts import render
 
@@ -12,6 +14,20 @@ from articleapp.models import Article
 from likeapp.models import LikeRecord
 
 
+@transaction.atomic
+def db_transaction(user, article):
+    like_record = LikeRecord.objects.filter(user=user,
+                                            article=article)
+
+    if like_record.exists():
+        raise ValidationError('like already exists.')
+    else:
+        LikeRecord(user=user, article=article).save()
+    article.like += 1
+    article.save()
+
+
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
 
@@ -19,19 +35,13 @@ class LikeArticleView(RedirectView):
         user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record = LikeRecord.objects.filter(user=user,
-                                                article=article)
-
-        if like_record.exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다!')
+        except:
             messages.add_message(request, messages.ERROR, '좋아요는 한번만!')
             return HttpResponseRedirect(reverse('articleapp:detail',
                                                 kwargs={'pk': kwargs['article_pk']}))
-        else:
-            LikeRecord(user=user, article=article).save()
-        article.like += 1
-        article.save()
-        messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다!')
-
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
