c0c356351d1a1f4e281a789e5f5ddb02c4be428c
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-26 12:46:20 +0900

    gis 08/26 6ban reupload practice 3. hello world deleted

--- accountapp/views.py
+++ accountapp/views.py
@@ -18,27 +18,10 @@ from django.urls import reverse
 from articleapp.models import Article
 
 
-@login_required
-def hello_world(request):
-    if request.method == 'POST':
-
-        temp = request.POST.get('input_text')
-
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/create.html'
 
 
@@ -74,5 +57,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
--- gis_django_1/settings.py
+++ gis_django_1/settings.py
@@ -162,5 +162,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGIN_REDIRECT_URL = reverse_lazy('articleapp:list')
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
