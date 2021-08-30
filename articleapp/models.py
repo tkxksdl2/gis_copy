7bdc806afb51efce6a70fbe1406fbdc23340182f
tkxksdl2 <79143006+tkxksdl2@users.noreply.github.com>
2021-08-30 11:18:36 +0900

    gis 08/30 6ban reupload practice 1.

--- articleapp/models.py
+++ articleapp/models.py
@@ -9,7 +9,7 @@ class Article(models.Model):
     writter = models.ForeignKey(User, on_delete=models.SET_NULL,
                                 related_name='article', null=True)
     project = models.ForeignKey(Project, on_delete=models.SET_NULL,
-                                related_name='project', null=True)
+                                related_name='project', null=True, blank=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to='article/', null=True)
     content = models.TextField(null=True)
